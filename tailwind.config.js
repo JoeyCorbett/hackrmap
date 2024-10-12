c9197892dddfab80f879d24c72ac4acbd78044b6
linhle15-ll <le224l@mtholyoke.edu>
2024-10-12 00:34:58 -0400

    front-end setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
+  darkMode: "class",
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#ffffff",
+        gray: {
+          100: "#fef4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          500: "#6b7280",
+          700: "#374151",
+          800: "#1f2937",
+        },
+        blue: {
+          200: "#93c5fd",
+          300: "#60a5fa",
+          400: "#3b82f6",
+          500: "#2563eb",
+          600: "#1d4ed8",
+        },
+        "dark-bg": "#101214",
+        "dark-bg-secondary": "#1d1f21",
+        "dark-tertiary": "#3b3d40",
+        "blue-primary": "#0275ff",
+        "stroke-dark": "#2d3135",
+        "stroke-light": "#f0f0f0",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": 
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      }
+    },
   },
   plugins: [],
 };
